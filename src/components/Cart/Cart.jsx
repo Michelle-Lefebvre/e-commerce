0bc3f558d5c08b238aca4f9f38273c8cef591209
Michelle <michelle.lefebvre@valtech.com>
2021-03-01 15:40:55 -0500

    add all button functions to Cart

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -4,13 +4,13 @@ import useStyles from './styles';
 import CartItem from './CartItem/CartItem';
 import { Link } from 'react-router-dom';
 
-const Cart = ({ cart }) => {
+const Cart = ({ cart, handleUpdateCartQty, handleRemoveFromCart, handleEmptyCart }) => {
 
     const classes = useStyles();
 
     const EmptyCart = () => (
        <Typography variant="subtitle1">You have no items in your shopping cart,
-            <Link to="/" className={classes.link}>start adding some!</Link>
+            <Link to="/" className={classes.link}> start adding some!</Link>
        </Typography>
     );
 
@@ -19,14 +19,14 @@ const Cart = ({ cart }) => {
             <Grid container spacing={3}>
                 {cart.line_items.map((item) => (
                     <Grid item xs={12} sm={4} key={item.id}>
-                        <CartItem item={item} />
+                        <CartItem item={item} onUpdateCartQty={handleUpdateCartQty} onRemoveFromCart={handleRemoveFromCart} />
                     </Grid>
                 ))}
             </Grid>
             <div className={classes.cardDetails}>
                 <Typography variant="h4">Subtotal: {cart.subtotal.formatted_with_symbol} </Typography>
                 <div>
-                    <Button className={classes.emptyButton} size="large" type="button" variant="container" color="secondary">Empty Cart</Button>
+                    <Button className={classes.emptyButton} size="large" type="button" variant="container" color="secondary" onClick={handleEmptyCart}>Empty Cart</Button>
                     <Button className={classes.checkoutButton} size="large" type="button" variant="container" color="primary">Checkout</Button>
                 </div>
             </div>
